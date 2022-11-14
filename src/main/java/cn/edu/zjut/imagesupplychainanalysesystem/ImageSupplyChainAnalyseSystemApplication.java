c6a26bd708cc7f54508d571135ed8e66dd19f657
zhuzhiling <917637197@qq.com>
2022-11-14 11:08:37 +0800

    first

---
+++ src/main/java/cn/edu/zjut/imagesupplychainanalysesystem/ImageSupplyChainAnalyseSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.imagesupplychainanalysesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSupplyChainAnalyseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSupplyChainAnalyseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/zjut/imagesupplychainanalysesystem/ImageSupplyChainAnalyseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.imagesupplychainanalysesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSupplyChainAnalyseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
