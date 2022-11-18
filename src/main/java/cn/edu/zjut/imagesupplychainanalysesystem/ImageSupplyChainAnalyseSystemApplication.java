7157ef7eed40f618aeecfee9fd7c74863d77835a
KongHaobin <1436351226@qq.com>
2022-11-18 15:36:54 +0800

    namespace test

--- src/main/java/cn/edu/zjut/imagesupplychainanalysesystem/ImageSupplyChainAnalyseSystemApplication.java
+++ src/main/java/cn/edu/zjut/imagesupplychainanalysesystem/ImageSupplyChainAnalyseSystemApplication.java
@@ -2,8 +2,13 @@ package cn.edu.zjut.imagesupplychainanalysesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableNeo4jRepositories
 public class ImageSupplyChainAnalyseSystemApplication {
 
 	public static void main(String[] args) {
